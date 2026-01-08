eac0250652959016f68fc688c5632e478984d12d
Ijeoma Mayor <ijeomamayor2004@gmail.com>
2026-01-09 00:12:16 +0100

    Move addToCart into cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,22 @@
 export const cart = [];
+
+export function addToCart(productId) {
+  let matchingItem;
+
+  /* This loop is Checking if there is a product with thesame name already available */
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  /* This is now increasing the quantity if the product already exists */
+  if (matchingItem) {
+    matchingItem.quantity += 1;
+  } else {
+    cart.push({
+      productId: productId,
+      quantity: 1
+    });
+  }
+}
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import {cart, addToCart} from '../data/cart.js';
 import { products } from '../data/products.js';
 
 let productsHTML ='';
@@ -56,36 +56,21 @@ products.forEach((product) => {
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
+function updateCartQuantity() {
+  let cartQuantity = 0;
+
+  /* This loop is calculating the total number of quantities in the cart for js-cart-quantity display on amazon.html*/
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
+
+  document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+}
+
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
   button.addEventListener('click', () => {
     const productId = button.dataset.productId;
-
-    let matchingItem;
-
-    /* This loop is Checking if there is a product with thesame name already available */
-    cart.forEach((item) => {
-      if (productId === item.productId) {
-        matchingItem = item;
-      }
-    });
-
-    /* This is now increasing the quantity if the product already exists */
-    if (matchingItem) {
-      matchingItem.quantity += 1;
-    } else {
-      cart.push({
-        productId: productId,
-        quantity: 1
-      });
-    }
-
-    let cartQuantity = 0;
-
-    /* This loop is calculating the total number of quantities in the cart for js-cart-quantity display on amazon.html*/
-    cart.forEach((item) => {
-      cartQuantity += item.quantity;
-    });
-
-    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+    addToCart(productId);
+    updateCartQuantity();
   });
 });
