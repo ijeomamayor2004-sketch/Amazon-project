954abebeb197dc6e9ef5a1499e97c444bb12f0c5
Ijeoma Mayor <ijeomamayor2004@gmail.com>
2026-01-09 11:20:11 +0100

    Delete products from HTML when clicking delete

--- data/cart.js
+++ data/cart.js
@@ -27,7 +27,7 @@ export function addToCart(productId) {
   }
 }
 
-export function reomoveFromCart(productId) {
+export function removeFromCart(productId) {
   const newCart = [];
 
   cart.forEach((cartItem) => {
