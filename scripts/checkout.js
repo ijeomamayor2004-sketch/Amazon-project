490968ded357652a45a2940a297d88063e1951e5
Ijeoma Mayor <ijeomamayor2004@gmail.com>
2026-02-06 15:16:50 +0100

    Added backend practice file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,6 +3,7 @@ import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { checkoutHeader } from "./checkout/checkoutHeader.js";
 // import '../data/car.js';
 // import '../data/cart-class.js';
+import '../data/backend-practice.js';
 
 checkoutHeader();
 renderPaymentSummary();
