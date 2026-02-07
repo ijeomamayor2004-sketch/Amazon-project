d77c26f916d956af52a0486bd73ee11d6c903327
Ijeoma Mayor <ijeomamayor2004@gmail.com>
2026-02-07 10:59:35 +0100

    Load products from the backend

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,89 +1,94 @@
 import {cart, addToCart, calculateCartQuantity} from '../data/cart.js';
-import { products } from '../data/products.js';
+import { products, loadProducts } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
-let productsHTML ='';
+loadProducts(renderProductsGrid);
 
-products.forEach((product) => {
-  productsHTML += `
-    <div class="product-container">
-      <div class="product-image-container">
-        <img class="product-image" src="${product.image}">
-      </div>
+function renderProductsGrid() {
+  let productsHTML ='';
 
-      <div class="product-name limit-text-to-2-lines">
-        ${product.name}
-      </div>
-
-      <div class="product-rating-container">
-        <img class="product-rating-stars" src="${product.getStarsUrl()}">
-        <div class="product-rating-count link-primary">
-          ${product.rating.count}
+  products.forEach((product) => {
+    productsHTML += `
+      <div class="product-container">
+        <div class="product-image-container">
+          <img class="product-image" src="${product.image}">
         </div>
-      </div>
 
-      <div class="product-price">
-        ${product.getPrice()}
-      </div>
+        <div class="product-name limit-text-to-2-lines">
+          ${product.name}
+        </div>
 
-      <div class="product-quantity-container">
-        <select class="js-quantity-selector-${product.id}">
-          <option selected value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-          <option value="6">6</option>
-          <option value="7">7</option>
-          <option value="8">8</option>
-          <option value="9">9</option>
-          <option value="10">10</option>
-        </select>
-      </div>
+        <div class="product-rating-container">
+          <img class="product-rating-stars" src="${product.getStarsUrl()}">
+          <div class="product-rating-count link-primary">
+            ${product.rating.count}
+          </div>
+        </div>
 
-      ${product.extraInfoHTML()}
+        <div class="product-price">
+          ${product.getPrice()}
+        </div>
 
-      <div class="product-spacer"></div>
+        <div class="product-quantity-container">
+          <select class="js-quantity-selector-${product.id}">
+            <option selected value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+          </select>
+        </div>
 
-      <div class="added-to-cart js-added-to-cart-${product.id}">
-        <img src="images/icons/checkmark.png">
-        Added
-      </div>
+        ${product.extraInfoHTML()}
 
-      <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
-        Add to Cart
-      </button>
-    </div>  
-  `;
-});
+        <div class="product-spacer"></div>
 
-document.querySelector('.js-products-grid').innerHTML = productsHTML;
+        <div class="added-to-cart js-added-to-cart-${product.id}">
+          <img src="images/icons/checkmark.png">
+          Added
+        </div>
 
-/* This funtion calculates the cart quantity and returns the numbered result into the variable which is used to display it on the page */
-function updateCartQuantity() {
-  const cartQuantity = calculateCartQuantity();
-
-  document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
-}
-
-updateCartQuantity();
-
-let timeoutId;
-
-function addedText(productId) {
-   /* This code displays added once the add to cart button is clicked */
-   document.querySelector(`.js-added-to-cart-${productId}`).classList.add('is-added-to-cart');
-   clearTimeout(timeoutId);
-   timeoutId = setTimeout( () => {
-     document.querySelector(`.js-added-to-cart-${productId}`).classList.remove('is-added-to-cart');
-   }, 2000);
-};
-
-document.querySelectorAll('.js-add-to-cart').forEach((button) => {
-  button.addEventListener('click', () => {
-    const productId = button.dataset.productId;
-    addedText(productId);
-    addToCart(productId);
-    updateCartQuantity();
+        <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
+          Add to Cart
+        </button>
+      </div>  
+    `;
   });
-});
+
+  document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+  /* This funtion calculates the cart quantity and returns the numbered result into the variable which is used to display it on the page */
+  function updateCartQuantity() {
+    const cartQuantity = calculateCartQuantity();
+
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+  }
+
+  updateCartQuantity();
+
+  let timeoutId;
+
+  function addedText(productId) {
+    /* This code displays added once the add to cart button is clicked */
+    document.querySelector(`.js-added-to-cart-${productId}`).classList.add('is-added-to-cart');
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout( () => {
+      document.querySelector(`.js-added-to-cart-${productId}`).classList.remove('is-added-to-cart');
+    }, 2000);
+  };
+
+  document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+    button.addEventListener('click', () => {
+      const productId = button.dataset.productId;
+      addedText(productId);
+      addToCart(productId);
+      updateCartQuantity();
+    });
+  });
+
+}
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,10 +1,13 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { checkoutHeader } from "./checkout/checkoutHeader.js";
+import { loadProducts } from "../data/products.js";
 // import '../data/car.js';
 // import '../data/cart-class.js';
-import '../data/backend-practice.js';
+// import '../data/backend-practice.js';
 
-checkoutHeader();
-renderPaymentSummary();
-renderOrderSummary();
+loadProducts(() => {
+  checkoutHeader();
+  renderPaymentSummary();
+  renderOrderSummary();
+});
