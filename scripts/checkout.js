b593eb440b2b5509a256df5441f3a90f145078bc
Ijeoma Mayor <ijeomamayor2004@gmail.com>
2026-02-08 17:02:17 +0100

    Learn promises and convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -6,8 +6,19 @@ import { loadProducts } from "../data/products.js";
 // import '../data/cart-class.js';
 // import '../data/backend-practice.js';
 
-loadProducts(() => {
+new Promise((resolve) => {
+  loadProducts(() => {
+    resolve();
+  });
+  
+}).then(() => {
   checkoutHeader();
   renderPaymentSummary();
   renderOrderSummary();
 });
+
+// loadProducts(() => {
+//   checkoutHeader();
+//   renderPaymentSummary();
+//   renderOrderSummary();
+// });
