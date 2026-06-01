c65b81cdbd05d8d4f2b098c73e2050fc16c6d596
Ijeoma Mayor <ijeomamayor2004@gmail.com>
2026-06-01 13:22:23 +0100

    Fixed broken links from renaming amazon.html to index.html

--- scripts/checkout/checkoutHeader.js
+++ scripts/checkout/checkoutHeader.js
@@ -1,5 +1,5 @@
 export function checkoutHeader() {
-  const renderCheckoutHeader = 'Checkout (<a class="return-to-home-link js-checkout-items" href="amazon.html"></a>)';
+  const renderCheckoutHeader = 'Checkout (<a class="return-to-home-link js-checkout-items" href="index.html"></a>)';
 
   document.querySelector('.js-checkout-header-middle-section').innerHTML = renderCheckoutHeader;
 }
